14e65fbc48183fdd686dbf69facd3dbe37f4fa17
Karthik <karthkn25.dev@gmail.com>
2025-05-17 18:33:28 +0530

    Body Component Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Body from "./components/Body";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="text-2xl flex justify-center items-center">
       <Header />
+      <Body/>
     </div>
   );
 }
